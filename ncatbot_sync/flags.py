a00652afc7355b6360b9a891576fed01da5d6037
木子 <2793415370@qq.com>
2025-03-31 22:24:20 +0800

    feat(Intents): 添加新的意图类型并重命名现有方法
    - 将现有的 none() 方法重命名为 public()
    - 新增 all()、group()、private()、notice() 和 request() 方法
    - 更新 should_handle 函数以适配新的意图类型

--- ncatbot_sync/flags.py
+++ ncatbot_sync/flags.py
@@ -8,9 +8,25 @@ class Intents:
         self.request_message = kwargs.get("request_message", False)
 
     @classmethod
-    def none(cls):
-        return cls(group_message=False, private_message=False, notice_message=False, request_message=False)
+    def public(self):
+        return self(group_message=True, private_message=True, notice_message=False, request_message=False)
 
+    def all(self):
+        return self(group_message=True, private_message=True, notice_message=True, request_message=True)
+    
+    def group(self):
+        return self(group_message=True, private_message=False, notice_message=False, request_message=False)
+    
+    def private(self):
+        return self(group_message=False, private_message=True, notice_message=False, request_message=False)
+    
+    def notice(self):
+        return self(group_message=False, private_message=False, notice_message=True, request_message=False)
+    
+    def request(self):
+        return self(group_message=False, private_message=False, notice_message=False, request_message=True)
+    
+    
 def should_handle(message, intents):
     if isinstance(message, GroupMessage):
         return intents.group_message
